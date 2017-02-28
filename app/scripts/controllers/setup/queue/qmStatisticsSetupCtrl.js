3c0e28fdd9786e7a3e823397ecc09b568ca244cf
kimey95.kim <kimey95.kim@ctf1.stw.net>
2017-02-28 13:36:25 +0900

    Queue management - Setup/submit - Search page

--- app/scripts/controllers/setup/queue/qmStatisticsSetupCtrl.js
+++ app/scripts/controllers/setup/queue/qmStatisticsSetupCtrl.js
@@ -32,7 +32,10 @@ kindFramework.controller('QMStatisticsCtrl', function (
 	var searchFromDate = null;
 	var searchToDate = null;
 
-	var drawColors = ["#1be2e4", "#31d70a", "#ff7f19"];
+	$scope.queueData = {};
+	$scope.queueData.dataLoad = false;
+
+	var channel = 0;
 
 	var cameraLocalTime = {
 		data: '',
@@ -53,6 +56,87 @@ kindFramework.controller('QMStatisticsCtrl', function (
 		getDateObj: function(){
 			return new Date(cameraLocalTime.data);
 		}
+	};
+
+	function getAttributes() {
+        var defer = $q.defer();
+
+        defer.resolve("success");
+        return defer.promise;
+    }
+
+	function setMaxResolution(){
+        return pcSetupService.setMaxResolution(mAttr.EventSourceOptions);
+	}
+
+	$scope.previewSection = {
+		coordinates: [],
+		goToSetup: function(){
+			$state.go("setup.statistics_queue_setup");
+		},
+		init: function(){
+			//sketchinfo
+			$scope.sketchinfo = {
+				shape: 1,
+				color: 0,
+				maxNumber: 3,
+				useEvent: false,
+				workType: 'qmArea',
+				modalId: "./views/setup/common/confirmMessage.html"
+			};
+
+			//coordinates
+			var datas = $scope.queueData.Queues;
+			for(var i = 0; i < datas.length; i++){
+				var points = [];
+				var data = datas[i].Coordinates;
+				for(var j = 0; j < data.length; j++){
+					points.push( [data[j].x, data[j].y] );
+				}
+
+	            $scope.coordinates.push(
+		            {
+	        			isSet: true,
+	        			enable: datas[i].Enable,
+	            		points: points,
+	            		textInCircle: (i + 1) + ''
+	            	}
+            	);
+			}
+		}
+	};
+
+	$scope.graphSection = {
+		start: function(type){
+			var successCallback = function(){
+
+			};
+
+			var data = {};
+
+			if(type === 'Today'){
+				data = {
+					Mode: 'Start',
+					"Queue.1.AveragePeople": true,
+					"Queue.2.AveragePeople": true,
+					"Queue.3.AveragePeople": true
+				};
+			}else{
+			}
+		}
+	};
+
+	$scope.searchSection = {
+		start: function(){
+
+		},
+		openReport: function(){
+
+		}
+	};
+
+	$scope.resultSection = {
+		view: false
 	};
 
 	function changeDateFormat(date, useMonth, useDay, useTime){
@@ -97,161 +181,42 @@ kindFramework.controller('QMStatisticsCtrl', function (
 		};
 	}
 
-	function setMaxResolution(){
-        return pcSetupService.setMaxResolution(mAttr.EventSourceOptions);
+	function controlSearch(data){
+		var dateForm = $scope.pcConditionsDateForm;
+
+        cameraLocalTime.set()
+        .then(
+        	function(){
+				var calenderDate = getCalenderDate();
+
+				data.Channel = channel;
+				data.FromDate = calenderDate.FromDate;
+				data.ToDate = calenderDate.ToDate;
+
+				qmModel.controlSearch(
+					data,
+					function(responseData){
+						if(responseData.SearchToken !== undefined){
+							$scope.searchToken = responseData.SearchToken;
+						}
+					},
+					function(errorData){
+						console.info(errorData);
+					}
+				);
+				// HMStatisticsModel
+				// 	.startSearch(searchFromDate, searchToDate, ResultImageType)
+				// 	.then(
+				// 		function(responseData){
+				// 			$scope.conditionsSection.checkSearch(withoutBG, responseData.SearchToken, 0);
+				// 		},
+				// 		function(failData){
+				// 			$scope.conditionsSection.errorSearch(failData);
+				// 		}
+				// 	);
+	        }
+	    );
 	}
-
-	function setRealtimeSize(){
-		var defaultResolution = pcSetupService.getDefaultResolution();
-		$(".qm-realtime-video").css({
-			minHeight: defaultResolution.height + "px"
-		});
-	}
-
-	function setResultImageSize(width, height){
-		$("#hm-results-image-1").css({
-			width: width + "px",
-			height: height + "px"
-		});
-	}
-
-	$scope.realTimeSection = {
-		goToSetup: function(){
-			$state.go("setup.statistics_queue_setup");
-		}
-	};
-
-	$scope.conditionsSection = {
-		startSearch: function(withoutBG){
-			// var dateForm = $scope.pcConditionsDateForm;
-			// if(asyncInterrupt || !$scope.useHeatmap || $scope.checkSearching || dateForm.validate() === false){
-			// 	return;
-			// }
-
-			// if(withoutBG){
-			// 	$scope.realTimeSection.hide();
-			// }else{
-			// 	$scope.checkSearching = true;
-			// 	$scope.resultSection.hide();
-			// }
-
-	  //       cameraLocalTime.set().then(function(){
-			// 	var calenderDate = getCalenderDate();
-			// 	searchFromDate = calenderDate.FromDate;
-			// 	searchToDate = calenderDate.ToDate;
-			// 	var ResultImageType = withoutBG ? "WithoutBackground" : "WithBackground";
-
-				// HMStatisticsModel.startSearch(searchFromDate, searchToDate, ResultImageType).then(
-				// 	function(responseData){
-				// 		$scope.conditionsSection.checkSearch(withoutBG, responseData.SearchToken, 0);
-				// 	},
-				// 	function(failData){
-				// 		$scope.conditionsSection.errorSearch(failData);
-				// 	}
-				// );
-	        // });
-		},
-		checkSearch: function(withoutBG, SearchToken, time){
-			// if(asyncInterrupt || time >= 60000){
-			// 	$scope.conditionsSection.cancelSearch(SearchToken);
-			// 	return;
-			// }
-
-			// HMStatisticsModel.checkSearch(SearchToken).then(
-			// 	function(responseData){
-			// 		console.log(responseData);
-			// 		if(responseData.Status === "Completed"){
-			// 			if(withoutBG){
-			// 			}else{
-			// 				$scope.resultSection.loadImage(SearchToken).then(
-			// 					function(successData){
-			// 						$scope.resultSection.show();
-			// 					},
-			// 					function(failData){
-			// 						$scope.conditionsSection.errorSearch(failData);
-			// 					}
-			// 				);
-			// 			}
-
-			// 			$scope.conditionsSection.cancelSearch(SearchToken);
-			// 		}else{
-			// 			time += 100;
-			// 			setTimeout(function(){
-			// 				$scope.conditionsSection.checkSearch(withoutBG, SearchToken, time);
-			// 			}, 100);
-			// 		}
-			// 	},
-			// 	function(failData){
-			// 		$scope.conditionsSection.errorSearch(failData);
-			// 	}
-			// );
-		},
-		cancelSearch: function(SearchToken){
-			// $scope.checkSearching = false;
-			/*
-			HMStatisticsModel.cancelSearch(SearchToken).then(
-				function(responseData){
-					console.log(responseData);
-				},
-				function(failData){
-					console.log(failData);
-				}
-			);
-			*/
-		},
-		errorSearch: function(failData){
-			// console.log(failData);
-			// $scope.checkSearching = false;
-			// openAlert({
-			// 	title: $translate.instant('lang_heatmap'),
-			// 	message: $translate.instant('lang_msg_heatmap_error')
-			// });
-		},
-		openReport : function(){
-			// openAlert({
-			// 	view: 'views/setup/heatmap/modals/report.html',
-			// 	controller: 'hmReportModalCtrl',
-			// 	size: 'md',
-			// 	data: {
-			// 		CameraName: function(){
-			// 			return $scope.deviceName;
-			// 		},
-			// 		FromDate: function(){
-			// 			return searchFromDate;
-			// 		},
-			// 		ToDate: function(){
-			// 			return searchToDate;
-			// 		}
-			// 	}
-			// });
-		}
-	};
-
-	$scope.resultSection = {
-		view: false,
-		show: function(){
-			// $scope.resultSection.view = true;
-		},
-		hide: function(){
-			// $scope.resultSection.view = false;
-		},
-		loadImage: function(SearchToken){
-			// var hmResultsImage = $("#hm-results-image-1");
-			// hmResultsImage.attr("src", HMStatisticsModel.getResultPath(SearchToken));
-
-			// var deferred = $q.defer();
-			
-			// hmResultsImage.on("load", function(){
-			// 	deferred.resolve(true);
-			// 	setResultImageSize(this.width, this.height);
-			// });
-			// hmResultsImage.on("error", function(){
-			// 	deferred.reject(false);
-			// });
-
-			// return deferred.promise;
-		}
-	};
 
 	function showVideo() {
         var getData = {};
@@ -347,69 +312,104 @@ kindFramework.controller('QMStatisticsCtrl', function (
 		return modalInstance.result;
 	}
 
-	var gaugeTimer = {};
-	$scope.gaugeSection = {
+    function getPercent(val, max){
+		return (val / max)*100;
+	}
+
+	function getPeopleData(id){
+		var max = $scope.queueData.Queues[id].MaxPeople;
+		var high = $scope.queueData.Queues[id].QueueTypes[0].Count;
+		var mid = Math.ceil( high / 2 );
+
+		return {
+			max: max,
+			high: high,
+			mid: mid
+		};
+	}
+
+	var gaugeTimer = {
+		timer0: null,
+		timer1: null,
+		timer2: null
+	};
+	$scope.queueLevelSection = {
+		maxArr: {},
 		start: function(id){
-			gaugeTimer[id] = setInterval(function(){
-				$scope.gaugeSection.change(id);
+			$scope.queueLevelSection.stop();
+			$scope.queueLevelSection.change(id);
+
+			gaugeTimer['timer' + id] = setInterval(function(){
+				$scope.queueLevelSection.change(id);
 			}, 3000);
 		},
 		stop: function(id){
-			if(gaugeTimer[id]){
-				clearInterval(gaugeTimer[id]);
-				delete gaugeTimer[id];
+			var name = 'timer' + id;
+			if(gaugeTimer[name] !== null){
+				clearInterval(gaugeTimer[name]);
+				gaugeTimer[name] = null;
 			}
 		},
 		change: function(id){
-			var colorList = ["#2beddb", "#0dd8eb", "#57ed06", "#0ec20e", "#ffab33", "#ff5400"];
-			var colorNameList = ["#1be2e4", "#31d70a", "#ff7f19"];
-			var parent = $("#qm-bar-"+id);
+			var successCallback = function(responseData){
+				var queue = responseData[id].Level;
+				var data = getPeopleData(id);
+				var parent = $("#qm-bar-" + id);
 
-			var mid = 3;
-			var high = 7;
-			var max = 10;
-			var rand = $scope.gaugeSection.makeRandom(0, max);
+				var colorList = ["#2beddb", "#0dd8eb", "#57ed06", "#0ec20e", "#ffab33", "#ff5400"];
+				var colorNameList = ["#1be2e4", "#31d70a", "#ff7f19"];
 
-			parent.find(".qm-bar-mask").css({
-				width: (100 - $scope.gaugeSection.getPercent(rand, max)) + "%"
-			});
+				parent.find(".qm-bar-mask").css({
+					width: (100 - getPercent(queue, data.max)) + "%"
+				});
 
-			//Bar 2
-			var colorName = null;
-			var startColor = null;
-			var endColor = null;
-			if(rand <= mid){
-				colorName = colorNameList[0];
-				startColor = colorList[0];
-				endColor = colorList[1];
-			}else if(rand <= high){
-				colorName = colorNameList[1];
-				startColor = colorList[2];
-				endColor = colorList[3];
-			}else{
-				colorName = colorNameList[2];
-				startColor = colorList[4];
-				endColor = colorList[5];
-			}
+				//Bar 2
+				var colorName = null;
+				var startColor = null;
+				var endColor = null;
+				if(queue < data.mid){
+					colorName = colorNameList[0];
+					startColor = colorList[0];
+					endColor = colorList[1];
+				}else if(queue < data.high){
+					colorName = colorNameList[1];
+					startColor = colorList[2];
+					endColor = colorList[3];
+				}else{
+					colorName = colorNameList[2];
+					startColor = colorList[4];
+					endColor = colorList[5];
+				}
 
-			parent.find(".qm-bar").css({
-				background: colorName,
-				background: "-webkit-linear-gradient(left, " + startColor + ", " + endColor + ")",
-				background: "-o-linear-gradient(right, " + startColor + ", " + endColor + ")",
-				background: "-moz-linear-gradient(right, " + startColor + ", " + endColor + ")",
-				background: "linear-gradient(to right, " + startColor + ", " + endColor + ")"
-			});
-		},
-		makeRandom: function(min, max){
-			var rand = Math.random() * (max- min) + min;
-			return Math.floor(rand);
-		},
-		getPercent: function(val, max){
-			return (val / max)*100;
+				parent.find(".qm-bar").css({
+					background: colorName,
+					background: "-webkit-linear-gradient(left, " + startColor + ", " + endColor + ")",
+					background: "-o-linear-gradient(right, " + startColor + ", " + endColor + ")",
+					background: "-moz-linear-gradient(right, " + startColor + ", " + endColor + ")",
+					background: "linear-gradient(to right, " + startColor + ", " + endColor + ")"
+				});
+			};
+
+			var failCallback = function(failData){
+				console.info(failData);
+			};
+
+			qmModel.checkData(
+				{
+					Channel: channel,
+					QueueIndex: (id + 1)
+				}
+			).then(successCallback, failCallback);
 		}
 	};
 
 	$scope.init = function(){
+		var deferred = $q.defer();
+		/**
+		 * When page is setted newly, Scroll of Browser have to set the top.
+		 */
+		$('.main.setup-wrapper').scrollTop(0);
+
 		if(setMaxResolution() === false){
 			console.error("Getting Maxinum Resolution of Video is Wrong!");
 		}
@@ -419,30 +419,33 @@ kindFramework.controller('QMStatisticsCtrl', function (
 			console.error(errorData);
 		};
 
-		setRealtimeSize();
-		$scope.gaugeSection.start(1);
-		$scope.gaugeSection.start(2);
-		$scope.gaugeSection.start(3);
+		qmModel.getData().then(
+			function(data){
+				$scope.queueData = data;
+				$scope.queueData.dataLoad = true;
+				console.info($scope.queueData);
 
-		$scope.pageLoaded = true;
-		// HMStatisticsModel.deviceInfo().then(
-		// 	function(successData){
-		// 		$scope.deviceName = successData.DeviceName;
-		// 		$scope.Model = successData.Model;
+				// Preview
+				$scope.previewSection.init();
+				// Queue Level(Start graph)
+				$scope.queueLevelSection.start(0);
+				$scope.queueLevelSection.start(1);
+				$scope.queueLevelSection.start(2);
 
-		// 		HMStatisticsModel.getReportInfo().then(
-		// 			function(data){
-		// 				$scope.useHeatmap = data.Enable;
-		// 				$scope.pcConditionsDateForm.init(function(){
-		// 					$scope.conditionsSection.startSearch(true);	
+				$scope.pcConditionsDateForm.init(function(){
+					//Graph
+					$scope.graphSection.start('Today');
+					$timeout(function(){
+						$scope.pageLoaded = true;
+					});
+				}, failCallback);
+			}, 
+			failCallback
+		);
 
-		// 					$timeout(function(){
-		// 						$scope.pageLoaded = true;	
-		// 					});
-		// 				}, failCallback);
-		// 			}, failCallback
-		// 		);
-		// 	}, failCallback);
+		deferred.resolve("Success");
+
+		return deferred.promise;
 	};
 
 	/* Destroy Area Start
@@ -458,18 +461,34 @@ kindFramework.controller('QMStatisticsCtrl', function (
     /* Queue management Search page를 벗어 날 때 */
     $scope.$on('$stateChangeStart',function (event, toState, toParams, fromState, fromParams) {
         if(fromState.controller === 'QMStatisticsCtrl'){
-			$scope.gaugeSection.stop(1);
-			$scope.gaugeSection.stop(2);
-			$scope.gaugeSection.stop(3);
+			$scope.queueLevelSection.stop(0);
+			$scope.queueLevelSection.stop(1);
+			$scope.queueLevelSection.stop(2);
 			gaugeTimer = {};
         }
     });
 
 	/* Destroy Area End
 	------------------------------------------ */
-
 	function view(){
-		showVideo().then($scope.init, $scope.init);
+		var failCallback = function(errorData){
+			console.log(errorData);
+			$scope.pageLoaded = true;
+		};
+
+		showVideo()
+			.then(
+				function(){
+					$scope.init()
+						.then(
+							function(){
+								$scope.pageLoaded = true;
+							}, 
+							failCallback
+						);
+				}, 
+				failCallback
+			);
 	}
 
 	$scope.view = view;
@@ -481,7 +500,9 @@ kindFramework.controller('QMStatisticsCtrl', function (
                 wait();
             }, 500);
         } else {
-        	view();
+            getAttributes().finally(function() {
+                view();
+            });
         }
 	})();
 });
