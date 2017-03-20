9c61f2123d66d46a35ead017ee5da98a94f10d95
mijoo462.park <mijoo462.park@hanwha.com>
2017-03-20 10:09:13 +0900

    timelineService error fixed (when endTarget = -1)

--- app/scripts/services/playbackClass/timelineService.js
+++ app/scripts/services/playbackClass/timelineService.js
@@ -937,7 +937,7 @@ kindFramework
 
 				this.resetTimeRange = function() {
 					var endTime = endTarget;
-					if( endTime !== null ) {
+					if( endTime !== null && endTime !== -1 ) {
 						endTime = convert_String_to_Date(endTime);
 						if( endTarget !== undefined && endTarget.substring(11,13) !== endTime.getHours()) {
 							endTime.checkDst(true);
