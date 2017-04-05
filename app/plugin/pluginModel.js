c6bc6401ee5131b48909d02081d72f57191e0e7d
jw3804.hong@ctf1.stw.net <jw3804.hong@ctf1.stw.net>
2017-04-05 11:31:25 +0900

    plugin package version 156

--- app/plugin/pluginModel.js
+++ app/plugin/pluginModel.js
@@ -4,12 +4,12 @@ kindFramework.factory('PluginModel', function() {
         NPAPI : {
             Name : 'Hanwha Techwin HTWisenetViewer Plugin',
             Description : 'HTWisenetViewer plugin',
-            Version: 155,
+            Version: 156,
             Path: 'plugin/HTWWisenetViewer.pkg'
         },
         ActiveX: {
             Name: 'Wisenet_ACTIVEX.wisenet_activexCtrl.1',
-            Version: 155,
+            Version: 156,
             Path: 'plugin/HTWWisenetViewer.exe'
         }
     };
