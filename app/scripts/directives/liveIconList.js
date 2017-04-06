008592c97de4b8ab2cb35fccb5b4ea77fc2fc0ff
byungin.song <byungin.song@hanwha.com>
2017-04-06 17:41:07 +0900

    [PLM_136521] Modified show ptz icon in live when user account login.

--- app/scripts/directives/liveIconList.js
+++ app/scripts/directives/liveIconList.js
@@ -310,6 +310,10 @@ kindFramework.directive('liveIconList', function(
 			function loadedAttr(){
 				scope.wisenetCameraFuntions2.ptz.show = (mAttr.ZoomOnlyModel || mAttr.PTZModel || mAttr.ExternalPTZModel || mAttr.isDigitalPTZ);
 
+				if (AccountService.isPTZAble() === false) {
+					scope.wisenetCameraFuntions2.ptz.show = false;
+				}
+
         if(mAttr.MaxAudioInput !== undefined)
         {
             scope.MaxAudioInput = mAttr.MaxAudioInput;
