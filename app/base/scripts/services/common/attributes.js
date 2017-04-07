49d0295b4c98cb7f71e2b884efa481fc1da12bbe
jin-gu.kim <jin-gu.kim@hanwha.com>
2017-04-07 10:41:23 +0900

    Modified Admin User authorization check

--- app/base/scripts/services/common/attributes.js
+++ app/base/scripts/services/common/attributes.js
@@ -1111,7 +1111,14 @@ kindFramework.service('Attributes', function ($timeout, $location, $q, SunapiCli
         }
         else
         {
-            return true;
+            if(SessionOfUserManager.getUsername()==='admin')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     };
 
