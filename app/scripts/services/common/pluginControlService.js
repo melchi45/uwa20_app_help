e79fa2d1cf16b631df3ba1f6a28cdef3e51e57ee
jin-gu.kim <jin-gu.kim@hanwha.com>
2017-04-13 15:49:01 +0900

    Modified AreaZoom Logic to fixed Bug

--- app/scripts/services/common/pluginControlService.js
+++ app/scripts/services/common/pluginControlService.js
@@ -391,8 +391,6 @@ kindFramework
     this.setAreaZoomAction = function(_command)
     {
       try{
-          if(PTZContorlService.getAreaZoomCheck()) return;
-
           switch (_command)
           {
               case '1X':
