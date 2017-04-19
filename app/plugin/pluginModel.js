8eca165d74182dcbf4313e129bd15bb84fde0a1e
dohyuns.kim <dohyuns.kim@hanwha.com>
2017-04-20 08:42:32 +0900

    resolve conflict

--- app/plugin/pluginModel.js
+++ app/plugin/pluginModel.js
@@ -4,20 +4,12 @@ kindFramework.factory('PluginModel', function() {
         NPAPI : {
             Name : 'Hanwha Techwin HTWisenetViewer Plugin',
             Description : 'HTWisenetViewer plugin',
-<<<<<<< HEAD
             Version: 302,
-=======
-            Version: 159,
->>>>>>> aaaf2254f78101a3bc3bd43dbdb622c76fcfa6de
             Path: 'plugin/HTWWisenetViewer.pkg'
         },
         ActiveX: {
             Name: 'Wisenet_ACTIVEX.wisenet_activexCtrl.1',
-<<<<<<< HEAD
             Version: 302,
-=======
-            Version: 159,
->>>>>>> aaaf2254f78101a3bc3bd43dbdb622c76fcfa6de
             Path: 'plugin/HTWWisenetViewer.exe'
         }
     };
