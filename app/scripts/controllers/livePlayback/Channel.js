a1cb220c066502753099dd1b0c18e96974883bb9
jin-gu.kim <jin-gu.kim@hanwha.com>
2017-04-26 14:46:11 +0900

    OverlayCanvas will be hidden for IE, MJPEG Mode

--- app/scripts/controllers/livePlayback/Channel.js
+++ app/scripts/controllers/livePlayback/Channel.js
@@ -951,7 +951,11 @@ kindFramework
     }
 
     $scope.checkPlugin = function() {
-      return UniversialManagerService.getStreamingMode() == CAMERA_STATUS.STREAMING_MODE.PLUGIN_MODE ? false : true;
+      if(UniversialManagerService.getStreamingMode() === CAMERA_STATUS.STREAMING_MODE.NO_PLUGIN_MODE)
+      {
+          return (BrowserService.BrowserDetect === BrowserService.BROWSER_TYPES.IE) ? false : true;
+      }
+      return true;
     };
 
     $rootScope.$saveOn("channel:setPlugin", function() {
