36388ffbdcf7879989be7bef5e49fc0ccdb084f1
jin-gu.kim <jin-gu.kim@hanwha.com>
2017-04-28 11:24:05 +0900

    Updated Web Plugin to 161 version
    Default setting of manual tracking is off

--- app/plugin/pluginModel.js
+++ app/plugin/pluginModel.js
@@ -4,12 +4,12 @@ kindFramework.factory('PluginModel', function() {
         NPAPI : {
             Name : 'Hanwha Techwin HTWisenetViewer Plugin',
             Description : 'HTWisenetViewer plugin',
-            Version: 160,
+            Version: 161,
             Path: 'plugin/HTWWisenetViewer.pkg'
         },
         ActiveX: {
             Name: 'Wisenet_ACTIVEX.wisenet_activexCtrl.1',
-            Version: 160,
+            Version: 161,
             Path: 'plugin/HTWWisenetViewer.exe'
         }
     };
