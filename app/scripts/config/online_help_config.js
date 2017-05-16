0eda0ebf9b9d57b7d190e5da08ef6802d1468d63
Yongku Cho <yongku7.cho@hanwha.com>
2017-05-16 13:14:00 +0900

    [Online Helper] RS485가 External PTZ와 같은 HTML명을 사용하여 예외 처리로  수정

--- app/scripts/config/online_help_config.js
+++ app/scripts/config/online_help_config.js
@@ -17,4 +17,27 @@ kindFramework
         ],
         WIDTH: 1024,
         HEIGHT: 800,
+        /**
+         * externalPTZ, RS485, RS485&422 페이지의 HTML파일이 동일하여
+         * 온라인 헬퍼 작성시 이슈 발생하였습니다.
+         * 온라인 헬퍼 오픈시 html 경로를 수정하여 오픈합니다.
+         *
+         * 포맷
+         *    REPLACE_TEMPLATE: {
+         *        <URL Name>: {
+         *            from: <From HTML Name>
+         *            to: <To HTML Name>
+         *        }
+         *    }
+         */
+        REPLACE_TEMPLATE: {
+            'rs485': {
+                from: 'externalPTZ',
+                to: 'rs485'
+            },
+            'rs485422': {
+                from: 'externalPTZ',
+                to: 'rs485422'
+            }
+        }
     });
