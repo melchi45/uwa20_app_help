64d23334b7e187a7399877610985d04ee05cdcb9
Yongku Cho <yongku7.cho@hanwha.com>
2017-06-13 21:02:10 +0900

    [TNB][Face Detection] Detection Line Color 추가

--- app/scripts/controllers/setup/analytics/faceDetectionController.js
+++ app/scripts/controllers/setup/analytics/faceDetectionController.js
@@ -68,6 +68,12 @@ kindFramework.controller(
       }
     });
 
+    $scope.selectedColorIndex = 0;
+    $scope.colorIndexList = [0,1,2,3,4,5,6,7,8];
+    $scope.selectColor = function(index){
+      $scope.selectedColorIndex = index;
+    };
+
     var startIndex = 2; //Temporary
     var endIndex = 9; //Temporary
 
